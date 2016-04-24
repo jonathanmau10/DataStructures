d9ca4e21ee558ef0e4c819e984586feb92d8e702
jonathan <jonathan@DESKTOP-HNG2OBR>
2016-04-24 16:52:46 -0500

    add more methods for class tree

---
+++ src/unity2/trees/Test.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unity2.trees;
+
+/**
+ *
+ * @author jonathan
+ */
+public class Test {
+    
+    public static void main(String args[]){
+        Tree t = new Tree();
+        int values[]={8,4,16,2,6,12,20,1,10,14,13,15};
+        for (int i = 0; i < values.length ; i++) {
+            t.add(values[i]);
+        }
+        int v = 15;
+        System.out.println("Path to "+v+":");
+        t.printPath(v);        
+        //---------------------------
+        if(t.isleaf(v)) {
+            System.out.println(" "+v+" is a leaf");
+        } else {
+            System.out.println(" "+v+" doesn't a leaf");
+        }
+        int c = t.count();
+        System.out.println("Total nodes: "+c);
+        
+        /*System.out.println("Print Leafs: ");
+        t.printLeafs();*/
+        t.printLeafs();
+            
+        System.out.println("Max: "+t.max());
+        
+        System.out.println("Min: "+t.min());  
+    }
+}
--- src/unity2/trees/Tree.java
+++ src/unity2/trees/Tree.java
@@ -1,6 +1,9 @@
 
 package unity2.trees;
 
+import java.util.Stack;
+import java.util.Vector;
+
 /**
  *
  * @author jonathan
@@ -35,5 +38,118 @@ public class Tree {
                 p.right = n;
             }
         } 
+    }
+    
+     public void print(){
+        Node aux=root;
+        Stack <Node> s = new Stack <Node> ();
+        while(aux!=null){
+            s.push(aux);
+            aux=aux.left;
+        }
+        while(!s.empty()){
+            aux=s.pop();
+            System.out.println(""+aux.data);
+            aux=aux.right;
+            while(aux!=null){
+                s.push(aux);
+                aux=aux.left;
+            }
+        }
+    }    
+     
+     public void printPath(int d){
+        Vector<Node> path = new Vector<Node>();
+        Node aux = root;
+        while(aux != null) {
+            path.add(aux);
+            if(d > aux.data) {
+                aux = aux.right;
+            } else if(d < aux.data) {
+                aux = aux.left;
+            } else {
+                for (int i = 0; i < path.size(); i++) {
+                    System.out.println(" "+path.elementAt(i).data);
+                }
+                System.out.println();
+                return;
+            }
+        }
+        System.out.println("Doesn't exist ");
+    }
+    
+    public boolean isleaf (int d){
+        Node aux = root;
+        while(aux != null){
+            if(d > aux.data){
+                aux = aux.right;
+            } else if(d < aux.data) {
+                aux = aux.left;
+            } else {
+                if(aux.right == null && aux.left == null){
+                return true;
+            } else {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+    public int count(){
+        int cont=0;
+        Node aux=root;
+        Stack <Node> s = new Stack <Node> ();
+        while(aux!=null){
+            s.push(aux);
+            aux=aux.left;
+        }
+        while(!s.empty()){
+            aux=s.pop();
+            cont++;
+            aux=aux.right;
+            while(aux!=null){
+                s.push(aux);
+                aux=aux.left;
+            }
+        }
+        return cont;
+    }
+    
+    public void printLeafs(){
+        Node aux = root;
+        Stack <Node> s = new Stack <Node> ();
+        while(aux != null) {
+            s.push(aux);
+            aux = aux.left;
+        }
+        while(!s.empty()) {
+            aux = s.pop();
+            aux = aux.right;
+            if(aux.left == null && aux.right == null) {
+                System.out.println(" "+aux.data);
+            }
+            while(aux != null) {
+                s.push(aux);
+                aux = aux.left;
+            }
+        }
+    }
+    
+    public int max(){
+        if(root==null)return -1;
+        Node aux=root;
+        while(aux.right!=null){
+            aux=aux.right;
+        }
+        return aux.data;
+    }
+    
+    public int min(){
+        if(root==null)return -1;
+        Node aux=root;
+        while(aux.left!=null) {
+            aux=aux.left;
+        }
+        return aux.data;
     }
 }
